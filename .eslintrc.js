0684539b3c7b2fbe45b83ceb1f101e0934e07fa1
Naufal Zufar <naufal.saifuddin@kupu.id>
2025-07-30 14:03:06 +0700

    feat: setup eslint exclude rules

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "next/typescript"],
+  rules: {
+    "no-unused-vars": "off",
+    "no-console": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "react/no-unescaped-entities": "off",
+    "react/display-name": "off",
+    "react/no-unknown-property": ["error", { ignore: ["jsx"] }],
+    "react/jsx-curly-brace-presence": ["warn", { props: "never", children: "never" }],
+    "@typescript-eslint/no-var-requires": "off",
+
+    //#region  //*=========== Unused Import ===========
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
